b897c6a056c293784efb79db13b81da5faf504d4
asriningtyas <asrityas37@gmail.com>
2024-06-23 17:45:45 +0700

    add master import assets

---
+++ constants/Audio.tsx
@@ -0,0 +1,5 @@
+const quizBacksound = require("@/assets/audio/quizBacksound.mp3");
+const correctBacksound = require("@/assets/audio/correct.mp3");
+const wrongBacksound = require("@/assets/audio/wrong.mp3");
+
+export { quizBacksound, correctBacksound, wrongBacksound };
---
+++ constants/Images.tsx
@@ -0,0 +1,59 @@
+const pen = require("@/assets/images/pen.png");
+const notebook = require("@/assets/images/notebook.png");
+const clip = require("@/assets/images/clip.png");
+const books = require("@/assets/images/books.png");
+const pencil = require("@/assets/images/pencil.png");
+const cabinet = require("@/assets/images/cabinet.png");
+const computer = require("@/assets/images/computer.png");
+const folder = require("@/assets/images/folder.png");
+const highlither = require("@/assets/images/highlither.png");
+const idcard = require("@/assets/images/idcard.png");
+const perforator = require("@/assets/images/perforator.png");
+const photocopiers = require("@/assets/images/photocopiers.png");
+const projector = require("@/assets/images/projector.png");
+const stepler = require("@/assets/images/stepler.png");
+const stickynotes = require("@/assets/images/stickynotes.png");
+const homeBackground = require("@/assets/images/home.jpg");
+const companyBackground = require("@/assets/images/company.jpg");
+const horensoAndSonkeigoBackground = require("@/assets/images/horensoAndSonkeigo.jpg");
+const quizHorensoAndSonkaigoBackground = require("@/assets/images/quizHorensoAndSonkaigo.jpg");
+const quizBackground = require("@/assets/images/quizcover.jpg");
+const quizKotobaBackground = require("@/assets/images/quizKotoba.jpg");
+const correctAnswer = require("@/assets/images/correct.png");
+const wrongAnswer = require("@/assets/images/wrong.png");
+const honda = require("@/assets/images/honda.jpg");
+const mitsubishi = require("@/assets/images/mitsubishi.jpeg");
+const nintendo = require("@/assets/images/nintendo.jpeg");
+const sony = require("@/assets/images/sony.jpg");
+const toyota = require("@/assets/images/toyota.png");
+
+export {
+  pen,
+  notebook,
+  clip,
+  books,
+  pencil,
+  cabinet,
+  computer,
+  folder,
+  highlither,
+  idcard,
+  perforator,
+  photocopiers,
+  projector,
+  stepler,
+  stickynotes,
+  homeBackground,
+  companyBackground,
+  quizHorensoAndSonkaigoBackground,
+  horensoAndSonkeigoBackground,
+  quizBackground,
+  quizKotobaBackground,
+  correctAnswer,
+  wrongAnswer,
+  honda,
+  mitsubishi,
+  nintendo,
+  sony,
+  toyota,
+};
