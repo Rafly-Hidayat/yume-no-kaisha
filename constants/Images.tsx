b1f7083acbcc2b1d5ae182f42eae5b6cbb7f346b
asriningtyas <asrityas37@gmail.com>
2024-06-29 10:42:51 +0700

    add assets

--- constants/Images.tsx
+++ constants/Images.tsx
@@ -26,6 +26,45 @@ const mitsubishi = require("@/assets/images/mitsubishi.jpeg");
 const nintendo = require("@/assets/images/nintendo.jpeg");
 const sony = require("@/assets/images/sony.jpg");
 const toyota = require("@/assets/images/toyota.png");
+const vocabularyBackground = require("@/assets/images/vocabularyBackground.jpg");
+const jobBackground = require("@/assets/images/jobBackground.jpg");
+const roomBackground = require("@/assets/images/roomBackground.jpg");
+const team = require("@/assets/images/team.png");
+const staff = require("@/assets/images/staff.png");
+const permanentEmployee = require("@/assets/images/permanentEmployee.png");
+const employee1 = require("@/assets/images/employee1.png");
+const employee2 = require("@/assets/images/employee2.png");
+const coworkers = require("@/assets/images/coworkers.png");
+const ceo = require("@/assets/images/ceo.png");
+const generalChairman = require("@/assets/images/generalChairman.png");
+const headOfDapartment = require("@/assets/images/headOfDapartment.png");
+const headOfWitness = require("@/assets/images/headOfWitness.png");
+const secretary = require("@/assets/images/secretary.png");
+const leader = require("@/assets/images/leader.png");
+const directorManager = require("@/assets/images/directorManager.png");
+const viceDirector = require("@/assets/images/viceDirector.png");
+const director = require("@/assets/images/director.png");
+const smokingRoom = require("@/assets/images/smokingRoom.png");
+const waitingRoom = require("@/assets/images/waitingRoom.png");
+const breakRoom = require("@/assets/images/breakRoom.png");
+const meetingRoom = require("@/assets/images/meetingRoom.png");
+const officeRoom = require("@/assets/images/officeRoom.png");
+const company = require("@/assets/images/company.png");
+const receptionist = require("@/assets/images/receptionist.png");
+const conversation1 = require("@/assets/images/conversation1.png");
+const conversation2 = require("@/assets/images/conversation2.png");
+const conversation3 = require("@/assets/images/conversation3.png");
+const sentencesAndConversations = require("@/assets/images/SentencesAndConversations.jpg");
+const roomIcon = require("@/assets/images/roomIcon.jpg");
+const jobIcon = require("@/assets/images/jobIcon.png");
+const officeToolIcon = require("@/assets/images/officeToolIcon.png");
+const horensoAndSonkeigoIcon = require("@/assets/images/horensoAndSonkeigoIcon.png");
+const sentenceIcon = require("@/assets/images/sentenceIcon.png");
+const conversationIcon = require("@/assets/images/conversationIcon.png");
+const quizIcon = require("@/assets/images/quizIcon.png");
+const phone = require("@/assets/images/phone.png");
+const tape = require("@/assets/images/tape.png");
+const marker = require("@/assets/images/marker.png");
 
 export {
   pen,
@@ -56,4 +95,43 @@ export {
   nintendo,
   sony,
   toyota,
+  vocabularyBackground,
+  jobBackground,
+  roomBackground,
+  team,
+  staff,
+  permanentEmployee,
+  employee1,
+  employee2,
+  coworkers,
+  ceo,
+  generalChairman,
+  headOfDapartment,
+  headOfWitness,
+  secretary,
+  leader,
+  directorManager,
+  viceDirector,
+  director,
+  smokingRoom,
+  waitingRoom,
+  breakRoom,
+  meetingRoom,
+  officeRoom,
+  company,
+  receptionist,
+  conversation1,
+  conversation2,
+  conversation3,
+  sentencesAndConversations,
+  roomIcon,
+  jobIcon,
+  officeToolIcon,
+  horensoAndSonkeigoIcon,
+  sentenceIcon,
+  conversationIcon,
+  quizIcon,
+  phone,
+  tape,
+  marker,
 };
